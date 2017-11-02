bdfae03e1ae28f52c8f3caaa35669525569ac0db
templeChange <229808018@qq.com>
2017-11-03 00:16:08 +0800

    更新了各个表的dao

--- src/test/java/user/service/TestUserService.java
+++ src/test/java/user/service/TestUserService.java
@@ -1,6 +1,7 @@
 package user.service;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestUserService {
@@ -9,8 +10,10 @@ public class TestUserService {
 	
 	@Before
 	public void init(){
-		
+		ctx=new ClassPathXmlApplicationContext("spring-mybatis.xml");
 	}
-	
+	@Test
+	public void test1(){
+	}
 	
 }
